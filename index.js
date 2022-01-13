633b614df64c761aa78897db91b4256d00f7fd71
garde1 <97448335+garde1@users.noreply.github.com>
2022-01-13 12:08:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const client = new Client({
   restTimeOffset: 0
 });
 
-client.login(TOKEN);
+client.login(OTMxMTAzNzMwNDQxNTMxNDYy.Yd_j7A.0fp5-qKOFWMYjFwu6jVDvJxVCaQ);
 client.commands = new Collection();
 client.prefix = PREFIX;
 client.queue = new Map();
